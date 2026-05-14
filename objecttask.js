9a32f59012db6238b9c823643a688bbbccbb53dc
johnkahiga <johnkhaiga27@gmail.com>
2026-05-14 15:00:42 +0300

    update

---
+++ objecttask.js
@@ -0,0 +1,5 @@
+let my_arr = [23, "Jane", 560, ["Lesson", "Maths", {"currency" : "KES"}], 987, 76, "John"]
+console.log(my_arr[3][2]["currency"])
+console.log(my_arr[2])
+console.log(my_arr[3][1])
+my_arr[3][2].amount = 90
