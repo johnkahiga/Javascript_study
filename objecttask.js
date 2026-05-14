f107efb80d513cf9afd191b8ab70fb5bc26bfc6e
johnkahiga <johnkhaiga27@gmail.com>
2026-05-14 15:29:15 +0300

    objecttask

--- objecttask.js
+++ objecttask.js
@@ -2,5 +2,11 @@ let my_arr = [23, "Jane", 560, ["Lesson", "Maths", {"currency" : "KES"}], 987, 7
 console.log(my_arr[3][2]["currency"])
 console.log(my_arr[2])
 console.log(my_arr[3][1])
-my_arr[3][2].amount = 90
+my_arr[3][2].amount=90
 console.log(my_arr)
+let value=Number(String(my_arr[4]).split('').reverse().join(''))
+my_arr[4]=value
+console.log(my_arr)
+my_arr[6]='Jane'
+console.log(my_arr)
+
